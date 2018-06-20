72da1b4b8dafc5694a048b5500d84079ca66b701
Mandy <mandyjjoles@gmail.com>
2018-06-19 22:04:37 -0500

    third commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 
 	function newCrystals () {
 		var numbers = []
-			while(numbers.length < 4){
+			while(numbers.length < 5){
 			  var randomnumber = Math.floor(Math.random()*12)
 			  var found = false;
 			  for (var i=0; i< numbers.length; i++){
@@ -35,6 +35,44 @@ $(document).ready(function() {
 		}
 	}
 
-	
+	function newGame() {
+
+		counter = 0;
+		$('#yourScore').text(counter);
+
+		function randomIntFromInterval(min,max){
+		   	return Math.floor(Math.random()*(max-min+1)+min);
+			}
+
+		var numberToGuess = randomIntFromInterval(19,50);
+
+		$('.value').text(numberToGuess);
+
+
+		$('.crystalImage').on('click', function(){
+		    counter = counter + parseInt($(this).data('num'));
+		   
+		    $('#yourScore').text(counter);
+
+		    if (counter == numberToGuess){
+		      $('#status').text('You won!!!!');
+		      wins ++;
+		      $('#win').text(wins);
+		      console.log(wins)
+		      $('#crystals').empty();
+		      newCrystals();
+		      newGame();
+		        
+		    } else if ( counter > numberToGuess){
+		        $('#status').text('You lost!')
+		        losses ++;
+		        $('#loss').text(losses);
+		        console.log(losses)
+		        $('#crystals').empty();
+		        newCrystals();
+		        newGame();
+		    }
+		});
+	}
 
 });
