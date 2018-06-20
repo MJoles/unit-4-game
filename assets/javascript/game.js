5699b75b9c938112c1e0001056d48e591b3e0687
Mandy <mandyjjoles@gmail.com>
2018-06-19 20:39:20 -0500

    second commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+	crystals = ['assets/images/ruby.jpg','assets/images/diamond.jpg','assets/images/yellow.jpg','assets/images/emerald.jpg'];
+
+	var counter = 0;
+	var wins = 0;
+	var losses = 0;
+	$('#win').text(wins);
+	$('#loss').text(losses);
+	
+	newCrystals();
+	newGame();
+
+	function newCrystals () {
+		var numbers = []
+			while(numbers.length < 4){
+			  var randomnumber = Math.floor(Math.random()*12)
+			  var found = false;
+			  for (var i=0; i< numbers.length; i++){
+				if (numbers[i] == randomnumber){
+					found = true; break
+				}
+			  }
+			  if(!found)numbers[numbers.length]=randomnumber;
+			}
+		console.log(numbers);		
+
+		for (i = 0; i < numbers.length; i++) {
+			var imageCrystal = $('#crystals');
+			imageCrystal.attr('data-num', numbers[i]);
+			imageCrystal.attr('src', crystals[i]);
+			imageCrystal.attr('alt', 'crystals');
+			imageCrystal.addClass('crystalImage')
+			$('#crystals').append(imageCrystal);
+		}
+	}
+
+	
+
+});
